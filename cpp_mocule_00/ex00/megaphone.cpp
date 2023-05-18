673ebe55550d1dcea153c315f701725e1686c92c
jeongyunnim <jeongyun7521@gmail.com>
2023-05-18 15:14:29 +0900

    Makefile 생성 중

--- cpp_mocule_00/ex00/megaphone.cpp
+++ cpp_mocule_00/ex00/megaphone.cpp
@@ -1,23 +1,18 @@
-//왜 헤더파일은 생성하지 않는가?
-
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[])
 {
 	std::string	sentence;
-	int			i;
 
 	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
-	i = 1;
-	while (i < argc)
+	for (int i = 1; i < argc; i++)
 	{
 		sentence += argv[i];
-		i++;
 	}
 	for (std::string::iterator it = sentence.begin(); it != sentence.end(); it++)
 	{
